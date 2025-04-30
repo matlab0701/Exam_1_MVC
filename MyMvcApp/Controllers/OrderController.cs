aba999f23db40c8f57c6282e26b18f659292099c
unknown <abdunazarovmmatlabshoh@gmail.com>
2025-04-30 15:45:50 +0500

    second commit

--- MyMvcApp/Controllers/OrderController.cs
+++ MyMvcApp/Controllers/OrderController.cs
@@ -1,12 +1,16 @@
 using Domain.DTOs.Orders;
 using Domain.Filters;
+using Infrastructure.Data;
 using Infrastructure.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace MyMvcApp.Controllers;
 
-public class OrderController(IOrderService orderService) : Controller
+public class OrderController(IOrderService orderService, DataContext context) : Controller
 {
+    public SelectList Customers { get; set; }
     public async Task<IActionResult> Index(OrderFilter filter)
     {
         var result = await orderService.GetAllAsync(filter);
@@ -23,8 +27,14 @@ public class OrderController(IOrderService orderService) : Controller
         return View(result.Data);
     }
 
-    public IActionResult Create()
+    public async Task<IActionResult> CreateAsync()
     {
+        ViewBag.Customers = new SelectList(
+            await context.Customers.ToListAsync(),
+            "Id",
+            "FullName"
+        );
+
         return View();
     }
 
@@ -34,10 +44,10 @@ public class OrderController(IOrderService orderService) : Controller
         var result = await orderService.CreateAsync(request);
         if (!result.IsSuccess)
         {
-        ModelState.AddModelError(string.Empty, result.Message);
-        return View(request);
+            ModelState.AddModelError(string.Empty, result.Message);
+            return View(request);
         }
-            return RedirectToAction(nameof(Index));
+        return RedirectToAction(nameof(Index));
     }
 
 }
